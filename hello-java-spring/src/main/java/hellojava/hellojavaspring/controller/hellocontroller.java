0d7f10e8b46bece521a36911776d4924e0152a21
SUNG YU BIN <31871417+SungYuBin@users.noreply.github.com>
2021-03-24 06:52:44 +0900

    기초 MVC

--- hello-java-spring/src/main/java/hellojava/hellojavaspring/controller/hellocontroller.java
+++ hello-java-spring/src/main/java/hellojava/hellojavaspring/controller/hellocontroller.java
@@ -3,6 +3,7 @@ package hellojava.hellojavaspring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 //controller 는 그냥 연결하는 다리수준입니다, 데이터를 넘기고 뭐 그런정도죠
 @Controller
@@ -12,4 +13,13 @@ public class hellocontroller {
        model.addAttribute("second","what");
     return "second";
    }
+
+   @GetMapping("mvctest")
+    public String mvctest(@RequestParam("test") String string1, Model model)
+   {
+        model.addAttribute("test",string1);
+        return "mvctest";
+   }
+
+
 }
