073c8361c012c629809ed58bdfb25d8310755a9d
SUNG YU BIN <31871417+SungYuBin@users.noreply.github.com>
2021-04-05 17:25:49 +0900

    데이터 h2와 연결중 집가서 h2 설치 필요

--- hello-java-spring/src/main/java/hellojava/hellojavaspring/SpringConfig.java
+++ hello-java-spring/src/main/java/hellojava/hellojavaspring/SpringConfig.java
@@ -1,14 +1,26 @@
 package hellojava.hellojavaspring;
 
 
+import hellojava.hellojavaspring.repository.JdbcMemberRepository;
 import hellojava.hellojavaspring.repository.MemberRepository;
 import hellojava.hellojavaspring.repository.MemoryMemberRepository;
 import hellojava.hellojavaspring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    @Autowired
+    DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource)
+    {
+        this.dataSource=dataSource;
+    }
+
 
     @Bean //스프링 빈에 등록하라는 의미
     public MemberService memberService(){
@@ -17,7 +29,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-     return new MemoryMemberRepository();
+    // return new MemoryMemberRepository();
+     return new JdbcMemberRepository(dataSource);
     }
 
 
