a038f0152a6c078771849323ac72afa83b218ca1
SUNG YU BIN <31871417+SungYuBin@users.noreply.github.com>
2021-04-06 20:50:37 +0900

    새 업로드

--- hello-java-spring/src/main/java/hellojava/hellojavaspring/SpringConfig.java
+++ hello-java-spring/src/main/java/hellojava/hellojavaspring/SpringConfig.java
@@ -1,26 +1,14 @@
 package hellojava.hellojavaspring;
 
 
-import hellojava.hellojavaspring.repository.JdbcMemberRepository;
 import hellojava.hellojavaspring.repository.MemberRepository;
 import hellojava.hellojavaspring.repository.MemoryMemberRepository;
 import hellojava.hellojavaspring.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
-    @Autowired
-    DataSource dataSource;
-
-    public SpringConfig(DataSource dataSource)
-    {
-        this.dataSource=dataSource;
-    }
-
 
     @Bean //스프링 빈에 등록하라는 의미
     public MemberService memberService(){
@@ -29,8 +17,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-    // return new MemoryMemberRepository();
-     return new JdbcMemberRepository(dataSource);
+     return new MemoryMemberRepository();
     }
 
 
--- hello-java-spring/src/main/java/hellojava/hellojavaspring/repository/JdbcMemberRepository.java
+++ hello-java-spring/src/main/java/hellojava/hellojavaspring/repository/JdbcMemberRepository.java
@@ -1,174 +1,46 @@
 package hellojava.hellojavaspring.repository;
 
+
 import hellojava.hellojavaspring.domain.Member;
 
-import org.springframework.jdbc.datasource.DataSourceUtils;
-
 import javax.sql.DataSource;
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class JdbcMemberRepository implements MemberRepository {
+
     private final DataSource dataSource;
 
-    public JdbcMemberRepository(DataSource dataSource) {
-        this.dataSource = dataSource;
+    //생성자
+    public JdbcMemberRepository(DataSource dataSource)
+    {
+        this.dataSource=dataSource;
+      //  dataSource.getConnection()
     }
 
     @Override
-    public Member Save(Member member) {
-
+    public Member Save(Member value) {
         String sql = "insert into member(name) values(?)";
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null; //결과를 받는것
-
-        //김영한 강사님께서는 귀찮아서 이 방식을 고수하신다고  합니다.<?
-
-        try {
-            conn = getConnection(); //커넥션 가지고 오기
-            pstmt = conn.prepareStatement(sql,
-                    Statement.RETURN_GENERATED_KEYS); //
-
-            pstmt.setString(1, member.getName()); //?와 매칭됩니다
-
-            pstmt.executeUpdate(); //DB에 쿼리가 날아갑니다.
-            rs = pstmt.getGeneratedKeys(); //key 를 꺼낼수있습니다.
-
-
-            if (rs.next()) {
-                member.setId(rs.getLong(1));
-            } else {
-                throw new SQLException("id 조회 실패");
-            }
-            return member;
-        } catch (Exception e) {
-            throw new IllegalStateException(e);
-        } finally {
-            close(conn, pstmt, rs); //이게 중요합니다. 연결을 햇다면 끊어주세요 예전에 장애가 생기는 가장 큰 이유
-        }
+        return null;
     }
 
-    //조회
     @Override
     public Optional<Member> findById(Long id) {
-        String sql = "select * from member where id = ?";
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-
-
-        try {
-            conn = getConnection();
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setLong(1, id);
-            rs = pstmt.executeQuery();
-
-            if (rs.next()) {
-                Member member = new Member();
-                member.setId(rs.getLong("id"));
-                member.setName(rs.getString("name"));
-
-                return Optional.of(member);
-            } else {
-                return Optional.empty();
-            }
-
-
-        } catch (Exception e) {
-            throw new IllegalStateException(e);
-        } finally {
-            close(conn, pstmt, rs);
-        }
+        return Optional.empty();
     }
 
+    @Override
+    public Optional<Member> findByName(String name) {
+        return Optional.empty();
+    }
 
     @Override
     public List<Member> findAll() {
-        String sql = "select * from member";
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            conn = getConnection();
-            pstmt = conn.prepareStatement(sql);
-            rs = pstmt.executeQuery();
-            List<Member> members = new ArrayList<>();
-            while (rs.next()) {
-                Member member = new Member();
-                member.setId(rs.getLong("id"));
-                member.setName(rs.getString("name"));
-                members.add(member);
-            }
-            return members;
-        } catch (Exception e) {
-            throw new IllegalStateException(e);
-        } finally {
-            close(conn, pstmt, rs);
-        }
+        return null;
     }
 
     @Override
     public int Count() {
         return 0;
-    }
-
-    @Override
-    public Optional<Member> findByName(String name) {
-        String sql = "select * from member where name = ?";
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            conn = getConnection();
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setString(1, name);
-            rs = pstmt.executeQuery();
-            if (rs.next()) {
-                Member member = new Member();
-                member.setId(rs.getLong("id"));
-                member.setName(rs.getString("name"));
-                return Optional.of(member);
-            }
-            return Optional.empty();
-        } catch (Exception e) {
-            throw new IllegalStateException(e);
-        } finally {
-            close(conn, pstmt, rs);
-        }
-    }
-
-    private Connection getConnection() {
-        return DataSourceUtils.getConnection(dataSource);
-    }
-
-    private void close(Connection conn, PreparedStatement pstmt, ResultSet rs) {
-        try {
-            if (rs != null) {
-                rs.close();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        try {
-            if (pstmt != null) {
-                pstmt.close();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        try {
-            if (conn != null) {
-                close(conn);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void close(Connection conn) throws SQLException {
-        DataSourceUtils.releaseConnection(conn, dataSource);
     }
 }
